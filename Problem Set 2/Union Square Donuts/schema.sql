aadef4d53330b0f96d358efd0ad5a0421379e21f
farghalii <hamzafarghali25@gmail.com>
2025-02-03 04:17:17 +0200

    done

---
+++ Problem Set 2/Union Square Donuts/schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE ingredients(
+    "id" INTEGER,
+    "ingredient_name" TEXT,
+    "price" REAL,
+    PRIMARY KEY ("id")
+);
+CREATE TABLE donuts(
+    "id" INTEGER,
+    "ingredient_id" INTEGER,
+    "name" TEXT,
+    "gluten-free" NUMERIC,
+    "price" REAL,
+    PRIMARY KEY ("id"),
+    FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id")
+);
+CREATE TABLE orders(
+    "order_number" INTEGER,
+    "donut_id" INTEGER,
+    "customer_id" INTEGER,
+    PRIMARY KEY ("order_number"),
+    FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id"),
+    FOREIGN KEY ("customer_id") REFERENCES "customers" ("id")
+);
+CREATE TABLE customers(
+        "id" INTEGER,
+        "order_id" INTEGER,
+        "first_name" TEXT,
+        "last_name" TEXT,
+        "history" NUMERIC,
+        PRIMARY KEY ("id"),
+         FOREIGN KEY ("order_id") REFERENCES "orders" ("id")
+);
+ CREATE TABLE ingredients_of_donut(
+        "ingredient_id" INTEGER,
+         "donut_id" INTEGER,
+          PRIMARY KEY ("ingredient_id","donut_id"),
+           FOREIGN KEY ("ingredient_id") REFERENCES "ingredients" ("id")
+          FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id")
+ );
+CREATE TABLE orders_donuts (
+        "order_number" INTEGER,
+        "donut_id" INTEGER,
+        FOREIGN KEY ("order_number") REFERENCES "orders" ("order_no"),
+        FOREIGN KEY ("donut_id") REFERENCES "donuts" ("id")
+    );
